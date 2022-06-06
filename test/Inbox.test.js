de64c7a97a06bdd8c42f12c79fd923e1c1a2fa57
Rakibul Hossain <rakibul.h@samsung.com>
2022-06-06 17:53:51 +0600

    test

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -1,1 +1,15 @@
 // contract test code will go here
+const assert = require('assert')
+const Web3 = require('web3')
+const ganache = require('ganache-cli')
+
+const web3 = new Web3(ganache.provider());
+
+let accounts;
+beforeEach(async()=> {
+    accounts = await web3.eth.getAccounts();
+})
+
+it('fetches account', ()=> {
+    console.log(accounts);
+})
